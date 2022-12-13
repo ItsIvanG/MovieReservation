f739e12115ead2d420805ba4c9240a935ad1ff46
Ivan <itsivang@gmail.com>
2022-12-13 23:39:56 +0800

    HallManager imports cleanup

--- src/HallManager.java
+++ src/HallManager.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
